296dd17a93f1e61a252d6730eaaa9cf6fdfaffcc
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-09 15:21:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 def reverse_each_word(msg)
   rev_msg = ""
-  array_msg = msg.split
-  array_msg.each{|rev_word| rev_msg << rev_word.reverse}.join(" ")
+  msg.split.each{|rev_word| rev_msg << rev_word.reverse}
   rev_msg
 end
